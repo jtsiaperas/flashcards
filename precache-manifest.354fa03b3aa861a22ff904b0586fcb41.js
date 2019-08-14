99640e89c2b29f816615b7d699b1cdddccc4d3cd
J. Austin Tsiaperas <liketexas@gmail.com>
2019-08-13 21:01:35 -0500

    Updates

---
+++ precache-manifest.354fa03b3aa861a22ff904b0586fcb41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ff9201ae19bb027c7eb5",
+    "url": "/static/js/main.ff9201ae.chunk.js"
+  },
+  {
+    "revision": "d2e2212d0304f57dfea1",
+    "url": "/static/js/1.d2e2212d.chunk.js"
+  },
+  {
+    "revision": "ff9201ae19bb027c7eb5",
+    "url": "/static/css/main.3429dd7f.chunk.css"
+  },
+  {
+    "revision": "0b3035d04b47f1767846fc6f89dd9e4d",
+    "url": "/index.html"
+  }
+];
